96ec1d96a1e854aa822faa70f0d78a6873fe7a10
Anna <anna.moskalenko1@gmail.com>
2019-02-26 18:37:03 +0100

    Updates

---
+++ precache-manifest.68fe07bf7910c893ac64666923f22a39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cdac62a7e92126aabd8",
+    "url": "/Manyuta/movies/static/js/runtime~main.7cdac62a.js"
+  },
+  {
+    "revision": "0fa2835a9e60c80c8caf",
+    "url": "/Manyuta/movies/static/js/main.0fa2835a.chunk.js"
+  },
+  {
+    "revision": "3db0c4bd209732452742",
+    "url": "/Manyuta/movies/static/js/2.3db0c4bd.chunk.js"
+  },
+  {
+    "revision": "0fa2835a9e60c80c8caf",
+    "url": "/Manyuta/movies/static/css/main.53bf24d9.chunk.css"
+  },
+  {
+    "revision": "40500f0bc63566d46ab5f6e7c053dda7",
+    "url": "/Manyuta/movies/index.html"
+  }
+];
